7263f8abc8b8b508b9baf8993604e3728174838e
Matthew Lindsay <thespian42@gmail.com>
2015-12-02 17:37:08 -0500

    Can now tilt the camera around the map to view obscured tiles

---
+++ Card Game/Assets/Scripts/CameraTilt.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraTilt : MonoBehaviour {
+
+	public GameObject rotationPoint;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		transform.up = Vector3.forward;
+	
+		if (Input.GetKey ("down")) {
+			transform.RotateAround(rotationPoint.transform.position, Vector3.left, 20 * Time.deltaTime);
+		}
+
+		if (Input.GetKey ("up")) {
+			transform.RotateAround(rotationPoint.transform.position, Vector3.right, 20 * Time.deltaTime);
+		}
+
+		if (Input.GetKey ("left")) {
+			transform.RotateAround(rotationPoint.transform.position, Vector3.forward, 20 * Time.deltaTime);
+		}
+
+		if (Input.GetKey ("right")) {
+			transform.RotateAround(rotationPoint.transform.position, Vector3.back, 20 * Time.deltaTime);
+		}
+
+
+	}
+}
