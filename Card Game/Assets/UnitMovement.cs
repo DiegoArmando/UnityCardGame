e611cd485b6a62c004c91829c821f7f7dcf605d3
chenj30 <chenj30@rpi.edu>
2015-12-04 16:36:37 -0500

    unit movement menu

---
+++ Card Game/Assets/UnitMovement.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UnitMovement : MonoBehaviour {
+    
+    public GameObject buttons = null;
+    private bool selected = false;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        buttons.SetActive(selected);
+	}
+
+    void OnMouseOver() {
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (selected) { selected = false; }
+            else if (!selected) {  selected = true;}
+        }
+    }
+}
