0a8ffbacae96eec1b32313ea4e3cb47d45cae60a
chenj30 <chenj30@rpi.edu>
2015-12-04 17:54:03 -0500

    movement gui added

---
+++ Card Game/Assets/Movement.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour {
+
+    public GameObject unit = null;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+}
--- Card Game/Assets/UnitMovement.cs
+++ Card Game/Assets/UnitMovement.cs
@@ -4,7 +4,12 @@ using System.Collections;
 public class UnitMovement : MonoBehaviour {
     
     public GameObject buttons = null;
+    
     private bool selected = false;
+    private int unitPosX = Screen.width / 2;
+    private int unitPosY = Screen.height / 2;
+    private int buttonSize = 64;
+    private int unitSize = 40;
 
 	// Use this for initialization
 	void Start () {
@@ -13,7 +18,7 @@ public class UnitMovement : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        buttons.SetActive(selected);
+
 	}
 
     void OnMouseOver() {
@@ -21,6 +26,38 @@ public class UnitMovement : MonoBehaviour {
         {
             if (selected) { selected = false; }
             else if (!selected) {  selected = true;}
+        }
+    }
+
+    void Deselect()
+    {
+        if (selected) { selected = false; };
+    }
+
+    void OnGUI()
+    {
+        if (selected && GUI.Button(new Rect((unitPosX - buttonSize/2) - buttonSize, (unitPosY - buttonSize/2), buttonSize, buttonSize), "Left")) {
+            this.transform.position += (new Vector3(-1.0f, 0.0f, 0.0f));
+            selected = false;
+            unitPosX -= unitSize;
+        }
+        if (selected && GUI.Button(new Rect((unitPosX - buttonSize/2) + buttonSize, (unitPosY - buttonSize/2), buttonSize, buttonSize), "Right"))
+        {
+            this.transform.position += (new Vector3(1.0f, 0.0f, 0.0f));
+            selected = false;
+            unitPosX += unitSize;
+        }
+        if (selected && GUI.Button(new Rect((unitPosX - buttonSize/2), (unitPosY - buttonSize/2) - buttonSize, buttonSize, buttonSize), "Up"))
+        {
+            this.transform.position += (new Vector3(0.0f, 0.0f, 1.0f));
+            selected = false;
+            unitPosY -= unitSize;
+        }
+        if (selected && GUI.Button(new Rect((unitPosX - buttonSize/2), (unitPosY - buttonSize/2) + buttonSize, buttonSize, buttonSize), "Down"))
+        {
+            this.transform.position += (new Vector3(0.0f, 0.0f, -1.0f));
+            selected = false;
+            unitPosY += unitSize;
         }
     }
 }
